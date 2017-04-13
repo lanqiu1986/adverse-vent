f70cbc93322d6b559abe433408490dc3ef89d838
lanqiu1986 <lisuji1986@163.com>
2017-04-13 16:15:36 +0800

    implement event#new and create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,19 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+
+    redirect_to events_path
+  end
+
+  def event_params
+    params.require(:event).permit(:department, :title, :description)
+  end
 end
