bf0b7ed7f844aee766484d0372734816b4511f0b
lanqiu1986 <lisuji1986@163.com>
2017-04-13 16:24:48 +0800

    implement event#edit update delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,27 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to events_path, notice: "Update Success"
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    flash[:alert] = "Event deleted"
+    redirect_to events_path
+  end
+
   def event_params
     params.require(:event).permit(:department, :title, :description)
   end
